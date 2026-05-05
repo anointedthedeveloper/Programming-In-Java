9db78f67abc22d0e63737373f0894071372584be
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-05-05 10:46:06 +0100

    Initial Commit

---
+++ localvariable.java
@@ -0,0 +1,8 @@
+public class localvariable {
+    public static void main(String[] args) {
+        int num1 = 10; // Local variable num1
+        int num2 = 20; // Local variable num2
+        int sum = num1 + num2; // Local variable sum
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
