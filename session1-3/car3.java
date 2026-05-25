117ade5238188313f744e7ac624edaa34a2cf8d7
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-05-25 10:42:30 +0100

    Add AcceptNumbers class to demonstrate user input and loop termination

---
+++ session1-3/car3.java
@@ -0,0 +1,13 @@
+Class car {
+    Void Start() {
+        System.out.println("Car has started");
+    }
+}
+
+public class car3 {
+    public static void main(String[] args) {
+        car myCar = new car();
+        myCar.Start();
+    }
+
+}
--- session1-3/forloop.java
+++ session1-3/forloop.java
@@ -2,6 +2,7 @@ public class forloop {
     public static void main(String[] args) {
         for( ; ; ) { 
             System.out.println("This will go on and on");
+
         }
-    }
+    }   
 }
