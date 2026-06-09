3e43a8c41ed6ff8586c64cf174c1f8143def7077
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-06-09 10:59:37 +0100

    ic

---
+++ session4/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    int width;
+    int height;
+
+   public static void main (Strings args []) {
+    Rectangle r1 = new Rectangle();
+    Rectangle r2 = new Rectangle();
+    r1.width = 10; r1.height = 20;
+    r2.width = 6; r2.height = 9;
+   }
+}
