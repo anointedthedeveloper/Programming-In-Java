a7800ec1d2d4ea0bd5884c5fee35d4fcc4918339
anointedthedeveloper <anointedthedeveloper@gmail.com>
2026-07-14 16:50:39 +0100

    Keyboard

---
+++ somakeys/Keyboard.java
@@ -0,0 +1,22 @@
+package somakeys;
+
+class Keyboard {
+    //instance variables
+    String characters;
+    int numbers;
+    String mouseDirection;
+    String keys;
+
+    //instance methods
+
+    //input method
+    private void input(){
+        //accept input
+    }
+
+    //direction method
+    private void direction(){
+            //move mouse and track direction 
+    }
+
+}
